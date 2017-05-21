864160b3f217c273121ca5c9e8017f6a82d46dd7
Akim Savchenko Len <akim.savchenko@gmail.com>
2017-05-22 00:48:36 +0300

    One loop ready

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-var TextSearch = +prompt("Введите предложение", "мама ммыла раму");
+var entryText = prompt("Enter some text","This is A TEXT to Find a WorD in it");
+var entryWord = prompt("Enter a word to search","find");
+
+entryText = entryText.toLocaleLowerCase();
+entryWord = entryWord.toLocaleLowerCase();
+
+var entryArray = entryText.split(" ");
+
+
+for (i = 0; i < entryArray.length; i++) {
+    
+    if (entryArray[i] == entryWord) {
+        console.log("The text : '" + entryText + "' contains the word '" + entryWord + "'.");
+    }    
+}
+
+
+
+
+/*for (i = 0; i < entryArray.length; i++) {
+    
+    if (entryArray[i] !== entryWord) {
+        console.log("There is NO such word as '" + entryWord + "' in this text '" + entryText + "'.");
+    }  
+}*/
